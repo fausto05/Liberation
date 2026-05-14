8bac72d8af85da5037b8895f549beff1483c5e7b
Mariano Fanelli <marianofanelli03@gmail.com>
2026-05-14 12:18:50 -0300

    ProjectilePool de enemigos

--- Assets/Scripts/Gameplay/Projectile.cs
+++ Assets/Scripts/Gameplay/Projectile.cs
@@ -4,30 +4,38 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private int damage;
+    [SerializeField] private float lifetime = 3f; 
 
     private Vector2 direction;
+    private float timer;
 
     public void Initialize(Vector2 dir)
     {
         direction = dir.normalized;
+        timer = 0f; 
     }
 
     private void Update()
     {
-        transform.position +=
-            (Vector3)(direction * speed * Time.deltaTime);
+        transform.position += (Vector3)(direction * speed * Time.deltaTime);
+
+        timer += Time.deltaTime;
+        if (timer >= lifetime)
+            ReturnToPool();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        IDamageable damageable =
-            other.GetComponent<IDamageable>();
-
+        IDamageable damageable = other.GetComponent<IDamageable>();
         if (damageable != null)
         {
             damageable.TakeDamage(damage);
-
-            Destroy(gameObject);
+            ReturnToPool();
         }
+    }
+
+    private void ReturnToPool()
+    {
+        ProjectilePool.Instance.ReturnToPool(this);
     }
 }
