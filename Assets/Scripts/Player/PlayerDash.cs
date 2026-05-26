3cfa4cb932001ff62da198675b9db60ec863fc97
Fausto Busso <faustobusso@gmail.com>
2026-05-26 17:50:45 -0300

    Dash
    movimiento de dash

---
+++ Assets/Scripts/Player/PlayerDash.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerDash : MonoBehaviour
+{
+    [SerializeField] private float dashForce = 12f;
+    [SerializeField] private float dashDuration = 0.15f;
+    [SerializeField] private float dashCooldown = 1f;
+
+    private Rigidbody2D rb;
+    private PlayerMovement playerMovement;
+
+    private bool isDashing;
+    private bool canDash = true;
+
+    public bool IsDashing => isDashing;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    public void TryDash()
+    {
+        if (!canDash || isDashing)
+            return;
+
+        StartCoroutine(DashCoroutine());
+    }
+
+    private IEnumerator DashCoroutine()
+    {
+        canDash = false;
+        isDashing = true;
+
+        Vector2 dashDirection = playerMovement.LastMoveDirection.normalized;
+
+        rb.linearVelocity = dashDirection * dashForce;
+
+        yield return new WaitForSeconds(dashDuration);
+
+        isDashing = false;
+
+        yield return new WaitForSeconds(dashCooldown);
+
+        canDash = true;
+    }
+}
