768931674b4dfe83f0abd3a6faeba80ff584c190
Fausto Busso <faustobusso@gmail.com>
2026-06-04 21:55:58 -0300

    Sistema de guardado comenzado

---
+++ Assets/Scripts/SaveSystem/SaveData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[System.Serializable]
+public class SaveData
+{
+    public int missionIndex = 0;
+
+    public bool gameCompleted = false;
+}
---
+++ Assets/Scripts/SaveSystem/SaveSystem.cs
@@ -0,0 +1,32 @@
+using System.IO;
+using UnityEngine;
+
+public static class SaveSystem
+{
+    private static string SavePath =>
+        Application.persistentDataPath + "/save.json";
+
+    public static void Save(SaveData data)
+    {
+        string json = JsonUtility.ToJson(data, true);
+        File.WriteAllText(SavePath, json);
+    }
+
+    public static SaveData Load()
+    {
+        if (!File.Exists(SavePath))
+            return new SaveData();
+
+        string json = File.ReadAllText(SavePath);
+
+        return JsonUtility.FromJson<SaveData>(json);
+    }
+
+    public static void DeleteSave()
+    {
+        if (File.Exists(SavePath))
+        {
+            File.Delete(SavePath);
+        }
+    }
+}
