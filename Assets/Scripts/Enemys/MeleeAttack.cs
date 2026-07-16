036576b35559e26ecffac7c36700cdfc45c9502d
Mariano Fanelli <marianofanelli03@gmail.com>
2026-07-16 16:08:14 -0300

    Mas audios agregados

--- Assets/Scripts/Enemys/MeleeAttack.cs
+++ Assets/Scripts/Enemys/MeleeAttack.cs
@@ -2,19 +2,36 @@ using UnityEngine;
 
 public class MeleeAttack : EnemyAttack
 {
+    [Header("Audio")]
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip attackSound;
+
     private Animator animator;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+
+        
+    }
+
+    public void PlayAttackSound()
+    {
+        if (audioSource != null && attackSound != null)
+            audioSource.PlayOneShot(attackSound);
     }
 
     public override void TryAttack()
     {
-        if (player == null) return;
+        if (player == null)
+            return;
+
         if (Time.time >= lastAttackTime + stats.attackCooldown)
         {
             animator.SetTrigger("Attack");
+
+            
+
             lastAttackTime = Time.time;
         }
     }
@@ -22,12 +39,14 @@ public class MeleeAttack : EnemyAttack
     protected override void PerformAttack()
     {
         IDamageable damageable = player.GetComponent<IDamageable>();
+
         if (damageable != null)
         {
             damageable.TakeDamage(stats.damage);
         }
     }
 
+    
     public void OnMeleeHit()
     {
         PerformAttack();
